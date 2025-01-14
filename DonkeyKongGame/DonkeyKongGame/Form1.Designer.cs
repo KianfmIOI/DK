80538ef4dafc52bf39c88ad000fadbc1a63ed7e3
Kian Ghaffarian <kianfmioi@gmail.com>
2025-01-14 13:06:45 +0330

    added barrels

--- DonkeyKongGame/DonkeyKongGame/Form1.Designer.cs
+++ DonkeyKongGame/DonkeyKongGame/Form1.Designer.cs
@@ -36,6 +36,22 @@
             ladder1 = new PictureBox();
             ladder2 = new PictureBox();
             mario = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            pictureBox6 = new PictureBox();
+            pictureBox7 = new PictureBox();
+            ScoreTXT = new Label();
+            barrellBox = new PictureBox();
+            pictureBox8 = new PictureBox();
+            pictureBox9 = new PictureBox();
+            pictureBox10 = new PictureBox();
+            pictureBox11 = new PictureBox();
+            pictureBox12 = new PictureBox();
+            pictureBox13 = new PictureBox();
+            pictureBox14 = new PictureBox();
+            pictureBox15 = new PictureBox();
+            pictureBox16 = new PictureBox();
+            coin = new PictureBox();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
@@ -44,100 +60,301 @@
             ((System.ComponentModel.ISupportInitialize)ladder1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)ladder2).BeginInit();
             ((System.ComponentModel.ISupportInitialize)mario).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)barrellBox).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox10).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox11).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox12).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox13).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox14).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox15).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox16).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)coin).BeginInit();
             SuspendLayout();
             // 
             // pictureBox1
             // 
             pictureBox1.BackColor = Color.FromArgb(128, 64, 64);
-            pictureBox1.Location = new Point(93, 413);
+            pictureBox1.Location = new Point(180, 624);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(405, 25);
+            pictureBox1.Size = new Size(649, 25);
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
+            pictureBox1.Tag = "Ground";
+            pictureBox1.Click += pictureBox1_Click;
             // 
             // pictureBox2
             // 
             pictureBox2.BackColor = Color.FromArgb(128, 64, 64);
-            pictureBox2.Location = new Point(176, 307);
+            pictureBox2.Location = new Point(68, 502);
             pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(405, 25);
+            pictureBox2.Size = new Size(606, 25);
             pictureBox2.TabIndex = 0;
             pictureBox2.TabStop = false;
+            pictureBox2.Tag = "Ground";
             pictureBox2.Click += pictureBox2_Click;
             // 
             // pictureBox3
             // 
             pictureBox3.BackColor = Color.FromArgb(128, 64, 64);
-            pictureBox3.Location = new Point(12, 197);
+            pictureBox3.Location = new Point(230, 392);
             pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(405, 25);
+            pictureBox3.Size = new Size(491, 25);
             pictureBox3.TabIndex = 0;
             pictureBox3.TabStop = false;
+            pictureBox3.Tag = "Ground";
             pictureBox3.Click += pictureBox2_Click;
             // 
             // platformEnd
             // 
             platformEnd.BackColor = Color.FromArgb(128, 64, 64);
-            platformEnd.Location = new Point(318, 91);
+            platformEnd.Location = new Point(269, 170);
             platformEnd.Name = "platformEnd";
             platformEnd.Size = new Size(405, 25);
             platformEnd.TabIndex = 0;
             platformEnd.TabStop = false;
+            platformEnd.Tag = "Ground";
             platformEnd.Click += pictureBox2_Click;
             // 
             // ladder3
             // 
             ladder3.BackColor = Color.Yellow;
-            ladder3.Location = new Point(392, 91);
+            ladder3.Location = new Point(485, 286);
             ladder3.Name = "ladder3";
-            ladder3.Size = new Size(25, 110);
+            ladder3.Size = new Size(25, 131);
             ladder3.TabIndex = 1;
             ladder3.TabStop = false;
+            ladder3.Tag = "Ladder";
             // 
             // ladder1
             // 
             ladder1.BackColor = Color.Yellow;
-            ladder1.Location = new Point(474, 307);
+            ladder1.Location = new Point(567, 502);
             ladder1.Name = "ladder1";
-            ladder1.Size = new Size(24, 109);
+            ladder1.Size = new Size(24, 147);
             ladder1.SizeMode = PictureBoxSizeMode.StretchImage;
             ladder1.TabIndex = 1;
             ladder1.TabStop = false;
+            ladder1.Tag = "Ladder";
             // 
             // ladder2
             // 
             ladder2.BackColor = Color.FromArgb(255, 255, 128);
-            ladder2.Location = new Point(176, 197);
+            ladder2.Location = new Point(269, 392);
             ladder2.Name = "ladder2";
-            ladder2.Size = new Size(15, 114);
+            ladder2.Size = new Size(24, 135);
             ladder2.TabIndex = 2;
             ladder2.TabStop = false;
+            ladder2.Tag = "Ladder";
             ladder2.Click += ladder2_Click;
             // 
             // mario
             // 
             mario.BackColor = Color.Blue;
-            mario.Location = new Point(108, 375);
+            mario.Location = new Point(201, 583);
             mario.Name = "mario";
             mario.Size = new Size(25, 35);
             mario.TabIndex = 3;
             mario.TabStop = false;
+            mario.Tag = "Mario";
             mario.Click += mario_Click;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.BackColor = Color.Yellow;
+            pictureBox4.Location = new Point(329, 170);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(24, 141);
+            pictureBox4.TabIndex = 5;
+            pictureBox4.TabStop = false;
+            pictureBox4.Tag = "Ladder";
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.BackColor = Color.FromArgb(128, 64, 64);
+            pictureBox5.Location = new Point(201, 286);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(363, 25);
+            pictureBox5.TabIndex = 4;
+            pictureBox5.TabStop = false;
+            pictureBox5.Tag = "Ground";
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.BackColor = Color.Yellow;
+            pictureBox6.Location = new Point(538, 74);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(26, 121);
+            pictureBox6.TabIndex = 6;
+            pictureBox6.TabStop = false;
+            pictureBox6.Tag = "Ladder";
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.BackColor = Color.FromArgb(128, 64, 64);
+            pictureBox7.Location = new Point(433, 74);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(179, 25);
+            pictureBox7.TabIndex = 7;
+            pictureBox7.TabStop = false;
+            pictureBox7.Tag = "Ground";
+            // 
+            // ScoreTXT
+            // 
+            ScoreTXT.AutoSize = true;
+            ScoreTXT.Location = new Point(24, 29);
+            ScoreTXT.Name = "ScoreTXT";
+            ScoreTXT.Size = new Size(51, 20);
+            ScoreTXT.TabIndex = 8;
+            ScoreTXT.Text = "score: ";
+            // 
+            // barrellBox
+            // 
+            barrellBox.BackColor = Color.DarkSalmon;
+            barrellBox.Location = new Point(566, 49);
+            barrellBox.Name = "barrellBox";
+            barrellBox.Size = new Size(14, 19);
+            barrellBox.TabIndex = 9;
+            barrellBox.TabStop = false;
+            barrellBox.Tag = "Barrell";
+            // 
+            // pictureBox8
+            // 
+            pictureBox8.BackColor = Color.DarkSalmon;
+            pictureBox8.Location = new Point(649, 145);
+            pictureBox8.Name = "pictureBox8";
+            pictureBox8.Size = new Size(14, 19);
+            pictureBox8.TabIndex = 9;
+            pictureBox8.TabStop = false;
+            pictureBox8.Tag = "Barrell";
+            // 
+            // pictureBox9
+            // 
+            pictureBox9.BackColor = Color.DarkSalmon;
+            pictureBox9.Location = new Point(446, 145);
+            pictureBox9.Name = "pictureBox9";
+            pictureBox9.Size = new Size(14, 19);
+            pictureBox9.TabIndex = 9;
+            pictureBox9.TabStop = false;
+            pictureBox9.Tag = "Barrell";
+            pictureBox9.Click += pictureBox9_Click;
+            // 
+            // pictureBox10
+            // 
+            pictureBox10.BackColor = Color.DarkSalmon;
+            pictureBox10.Location = new Point(279, 261);
+            pictureBox10.Name = "pictureBox10";
+            pictureBox10.Size = new Size(14, 19);
+            pictureBox10.TabIndex = 10;
+            pictureBox10.TabStop = false;
+            pictureBox10.Tag = "Barrell";
+            // 
+            // pictureBox11
+            // 
+            pictureBox11.BackColor = Color.DarkSalmon;
+            pictureBox11.Location = new Point(482, 261);
+            pictureBox11.Name = "pictureBox11";
+            pictureBox11.Size = new Size(14, 19);
+            pictureBox11.TabIndex = 11;
+            pictureBox11.TabStop = false;
+            pictureBox11.Tag = "Barrell";
+            // 
+            // pictureBox12
+            // 
+            pictureBox12.BackColor = Color.DarkSalmon;
+            pictureBox12.Location = new Point(365, 367);
+            pictureBox12.Name = "pictureBox12";
+            pictureBox12.Size = new Size(14, 19);
+            pictureBox12.TabIndex = 12;
+            pictureBox12.TabStop = false;
+            pictureBox12.Tag = "Barrell";
+            pictureBox12.Click += this.pictureBox12_Click;
+            // 
+            // pictureBox13
+            // 
+            pictureBox13.BackColor = Color.DarkSalmon;
+            pictureBox13.Location = new Point(568, 367);
+            pictureBox13.Name = "pictureBox13";
+            pictureBox13.Size = new Size(14, 19);
+            pictureBox13.TabIndex = 13;
+            pictureBox13.TabStop = false;
+            pictureBox13.Tag = "Barrell";
+            pictureBox13.Click += this.pictureBox13_Click;
+            // 
+            // pictureBox14
+            // 
+            pictureBox14.BackColor = Color.DarkSalmon;
+            pictureBox14.Location = new Point(317, 477);
+            pictureBox14.Name = "pictureBox14";
+            pictureBox14.Size = new Size(14, 19);
+            pictureBox14.TabIndex = 14;
+            pictureBox14.TabStop = false;
+            pictureBox14.Tag = "Barrell";
+            // 
+            // pictureBox15
+            // 
+            pictureBox15.BackColor = Color.DarkSalmon;
+            pictureBox15.Location = new Point(520, 477);
+            pictureBox15.Name = "pictureBox15";
+            pictureBox15.Size = new Size(14, 19);
+            pictureBox15.TabIndex = 15;
+            pictureBox15.TabStop = false;
+            pictureBox15.Tag = "Barrell";
+            // 
+            // pictureBox16
+            // 
+            pictureBox16.BackColor = Color.DarkSalmon;
+            pictureBox16.Location = new Point(149, 477);
+            pictureBox16.Name = "pictureBox16";
+            pictureBox16.Size = new Size(14, 19);
+            pictureBox16.TabIndex = 9;
+            pictureBox16.TabStop = false;
+            pictureBox16.Tag = "Barrell";
+            // 
+            // coin
+            // 
+            coin.BackColor = Color.Gold;
+            coin.Location = new Point(526, 548);
+            coin.Name = "coin";
+            coin.Size = new Size(17, 23);
+            coin.TabIndex = 16;
+            coin.TabStop = false;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(192, 255, 255);
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1027, 689);
+            Controls.Add(coin);
+            Controls.Add(pictureBox14);
+            Controls.Add(pictureBox15);
+            Controls.Add(pictureBox12);
+            Controls.Add(pictureBox13);
+            Controls.Add(pictureBox10);
+            Controls.Add(pictureBox11);
+            Controls.Add(pictureBox9);
+            Controls.Add(pictureBox8);
+            Controls.Add(pictureBox16);
+            Controls.Add(barrellBox);
+            Controls.Add(ScoreTXT);
+            Controls.Add(platformEnd);
+            Controls.Add(pictureBox7);
+            Controls.Add(pictureBox6);
+            Controls.Add(pictureBox5);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(pictureBox4);
             Controls.Add(mario);
             Controls.Add(ladder2);
             Controls.Add(ladder1);
             Controls.Add(ladder3);
-            Controls.Add(platformEnd);
-            Controls.Add(pictureBox3);
-            Controls.Add(pictureBox2);
-            Controls.Add(pictureBox1);
             KeyPreview = true;
             Name = "Form1";
             Text = "DK";
@@ -150,7 +367,23 @@
             ((System.ComponentModel.ISupportInitialize)ladder1).EndInit();
             ((System.ComponentModel.ISupportInitialize)ladder2).EndInit();
             ((System.ComponentModel.ISupportInitialize)mario).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)barrellBox).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox10).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox11).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox12).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox13).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox14).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox15).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox16).EndInit();
+            ((System.ComponentModel.ISupportInitialize)coin).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -163,5 +396,21 @@
         private PictureBox ladder1;
         private PictureBox ladder2;
         private PictureBox mario;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox6;
+        private PictureBox pictureBox7;
+        private Label ScoreTXT;
+        private PictureBox barrellBox;
+        private PictureBox pictureBox8;
+        private PictureBox pictureBox9;
+        private PictureBox pictureBox10;
+        private PictureBox pictureBox11;
+        private PictureBox pictureBox12;
+        private PictureBox pictureBox13;
+        private PictureBox pictureBox14;
+        private PictureBox pictureBox15;
+        private PictureBox pictureBox16;
+        private PictureBox coin;
     }
 }
--- DonkeyKongGame/DonkeyKongGame/Form1.cs
+++ DonkeyKongGame/DonkeyKongGame/Form1.cs
@@ -2,12 +2,44 @@ namespace DonkeyKongGame
 {
     public partial class Form1 : Form
     {
+        bool goRight, goLeft, closeToLadder, isJumping, isGameOver;
+        int score = 0;
+        int bestScore = 0;
+        int marioSpeed = 5;
+        int jumpSpeed = 7;
+        int barrellFallSpeed = 8;
+        double barrellSpeed = 3.5;
         public Form1()
         {
             InitializeComponent();
         }
-
+        public void restartGame()
+        {
+            goLeft = false;
+            goRight = false;
+            isGameOver = false;
+            if (score > bestScore)
+            {
+                bestScore = score;
+            }
+            score = 0;
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && x.Visible == false)
+                {
+                    x.Visible = true;
+                }
+            }
+        }
         private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void pictureBox12_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void pictureBox13_Click(object sender, EventArgs e)
         {
 
         }
@@ -30,6 +62,16 @@ namespace DonkeyKongGame
         }
 
         private void ladder2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
         {
 
         }
